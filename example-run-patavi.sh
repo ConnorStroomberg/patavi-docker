0e1446cc912baae76884eb1e8bb5f3f294bf066e
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-18 10:10:45 +0200

    update run example with correct user

--- example-run-patavi.sh
+++ example-run-patavi.sh
@@ -1,6 +1,6 @@
 PATAVI_SERVER=$(docker run -d \
 	-e PATAVI_TASK_SILENCE_TIMEOUT=20000 \
 	-e PATAVI_TASK_GLOBAL_TIMEOUT=300000 \
-	-e PATAVI_CACHE_DB_URL="postgresql://localhost/addiscore?user=addiscore&password=develop" \
+	-e PATAVI_CACHE_DB_URL="postgresql://localhost/patavitask?user=patavitask&password=develop" \
 	--name patavi_server -p 3000:3000 patavi/server)
 PATAVI_WORKER=$(docker run -d --link patavi_server:patavi_server patavi/slow)
